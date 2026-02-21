45a6dc5b3ee33c64533a299bbd54d0daef789b21
= <abdallaroom25@gmail.com>
2026-02-21 11:05:40 +0200

    remove broken custom image downloads

--- onCreateCommand.sh
+++ onCreateCommand.sh
@@ -23,20 +23,7 @@ chmod +x /usr/local/bin/rebuild
 chmod +x /usr/local/bin/remove
 chmod +x /usr/local/bin/kill
 
-wget https://github.com/ItzLevvie/artifacts/releases/download/27924-1/data.7z.001 --output-document /tmp/$REPOSITORY/windows/data.7z.001
-wget https://github.com/ItzLevvie/artifacts/releases/download/27924-1/data.7z.002 --output-document /tmp/$REPOSITORY/windows/data.7z.002
-wget https://github.com/ItzLevvie/artifacts/releases/download/27924-1/data.7z.003 --output-document /tmp/$REPOSITORY/windows/data.7z.003
-wget https://github.com/ItzLevvie/artifacts/releases/download/27924-1/data.7z.004 --output-document /tmp/$REPOSITORY/windows/data.7z.004
 
-7z x /tmp/$REPOSITORY/windows/data.7z.001 -o/tmp/$REPOSITORY/windows
-rm --force /tmp/$REPOSITORY/windows/data.7z.00*
-qemu-img convert -p -O raw -o preallocation=off /tmp/$REPOSITORY/windows/data.vhdx /tmp/$REPOSITORY/windows/data.img
-rm --force /tmp/$REPOSITORY/windows/data.vhdx
-cp /tmp/$REPOSITORY/windows/data.img /workspaces/$REPOSITORY/windows/data.img
-
-{
-    echo "data.img"
-} > /workspaces/$REPOSITORY/windows/windows.boot
 
 {
     echo "services:"
